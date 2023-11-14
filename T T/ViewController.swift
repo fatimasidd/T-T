c279f9d08996905690d74e6b99a5b4745def6735
FATIMA SIDDIQUI <fatimaarshad27@gmail.com>
2023-11-14 15:11:58 +0300

    Getting started

---
+++ T T/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  T T
+//
+//  Created by Fatima Siddiqui on 30/04/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
